bc233c549980579e504cdf07a5f3ec9c13bf0700
jobin <jobinabr@gmail.com>
2024-10-20 14:56:26 +0530

    setup

---
+++ src/main/java/com/jobra/volumeprocessor/LargeVolumeApplication.java
@@ -0,0 +1,13 @@
+package com.jobra.volumeprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LargeVolumeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LargeVolumeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jobra/volumeprocessor/LargeVolumeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobra.volumeprocessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LargeVolumeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
